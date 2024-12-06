ca183811cf3a8b08d7bb65dd975fc6d04d56f8af
uniquesongbw@163.com <uniquesongbw@163.com>
2024-12-06 15:42:07 +0800

    进度条

--- src/Html/Loading.tsx
+++ src/Html/Loading.tsx
@@ -13,10 +13,11 @@ function Loading() {
   }, [progress])
   return (
     <>
-      <div className={classNames('w-full', 'bg-sky-100', 'h-full', 'absolute', ' -z-10', 'flex', progress === 100 && 'animate-fadeout')}>
+      <div className={classNames('w-full', 'pointer-events-auto', 'bg-sky-100', 'h-full', 'absolute', ' -z-10', 'flex', progress === 100 && 'animate-fadeout')}>
         <div className='m-auto w-full px-[5vw]' >
-          <div className='text-right text-purple-700 text-3xl font-mono '>{progress.toFixed(2)}</div>
-          <div style={{ transform: `scaleX(${progress / 100})` }} className='h-2 bg-purple-700 rounded-full transition-transform origin-left'></div>
+          <div style={{ width: progress + '%' }} className='h-2 bg-purple-700 rounded-full transition-[width] origin-left'>
+            <div className='text-right text-purple-700 text-3xl font-mono relative -top-9'>{progress.toFixed(2)}</div>
+          </div>
         </div >
       </div >
 
